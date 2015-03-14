9138000306ccd60bbf8c0cf34fb58c10c114fc9e
Clive C. Banditi <clive@cornelo.us>
2015-03-14 16:31:26 +0200

    Updated Easy WP SMTP and changed WP admin logo

--- content/themes/jobify-child/functions.php
+++ content/themes/jobify-child/functions.php
@@ -11,4 +11,17 @@
 function jobify_child_styles() {
     wp_enqueue_style( 'jobify-child', get_stylesheet_uri() );
 }
-add_action( 'wp_enqueue_scripts', 'jobify_child_styles', 20 );
+add_action( 'wp
+
+_enqueue_scripts', 'jobify_child_styles', 20 );
+
+function my_login_logo_one() {
+    ?>
+    <style type="text/css">
+        body.login div#login h1 a {
+            background-image: url(http://localhost/content/images/cv.jpg);
+            padding-bottom: 30px;
+        }
+    </style>
+<?php
+} add_action( 'login_enqueue_scripts', 'my_login_logo_one' );
