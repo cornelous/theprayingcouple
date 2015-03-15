7b2f102e4e3193c14d4811ddd2123b9903f01837
Clive C. Banditi <clive@cornelo.us>
2015-03-16 00:38:06 +0200

    WP Job Manager Google Adsense plugin is crap! :(

--- content/plugins/wp-job-manager/templates/job-listings-start.php
+++ content/plugins/wp-job-manager/templates/job-listings-start.php
@@ -1,1 +1,10 @@
 <ul class="job_listings">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- 728x90 -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:728px;height:90px"
+         data-ad-client="ca-pub-8405479344707225"
+         data-ad-slot="7355681593"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
