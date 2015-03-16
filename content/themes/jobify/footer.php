a97d7e9e8e6dbdbf27948a10d243a27f21d3e512
Clive C. Banditi <clive@cornelo.us>
2015-03-16 19:56:00 +0200

    Added StatCounter code

--- content/themes/jobify/footer.php
+++ content/themes/jobify/footer.php
@@ -70,5 +70,24 @@
     ga('send', 'pageview');
 
 </script>
+
+<!-- Start of StatCounter Code for Shopify -->
+<script type="text/javascript">
+    var sc_project=10345697;
+    var sc_invisible=0;
+    var sc_security="449ab7da";
+    var scJsHost = (("https:" == document.location.protocol) ?
+        "https://secure." : "http://www.");
+    document.write("<sc"+"ript type='text/javascript' src='" +
+        scJsHost+
+        "statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="shopify stats"
+                                      href="http://statcounter.com/shopify/" target="_blank"><img
+                class="statcounter"
+                src="http://c.statcounter.com/10345697/0/449ab7da/0/"
+                alt="shopify stats"></a></div></noscript>
+<!-- End of StatCounter Code for Shopify -->
+
 </body>
 </html>
