95039df8d2b596d50a8c91106331320d086bea34
Clive C. Banditi <clive@cornelo.us>
2015-03-16 23:03:03 +0200

    Put back adsense code

--- content/themes/jobify/sidebar-single-job_listing.php
+++ content/themes/jobify/sidebar-single-job_listing.php
@@ -43,11 +43,11 @@ $args = array(
 	<?php do_action( 'single_job_listing_info_after' ); ?>
 
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- 336x280 -->
+    <!-- 300x600 -->
     <ins class="adsbygoogle"
-         style="display:inline-block;width:336px;height:280px"
+         style="display:inline-block;width:300px;height:600px"
          data-ad-client="ca-pub-8405479344707225"
-         data-ad-slot="1079671995"></ins>
+         data-ad-slot="6986604797"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
