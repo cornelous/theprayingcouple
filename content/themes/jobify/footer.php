10520197d10cfaa486f35358f288e06beaf87bca
Clive C. Banditi <clive@cornelo.us>
2015-03-26 04:15:56 +0200

    Added new Analytics code

--- content/themes/jobify/footer.php
+++ content/themes/jobify/footer.php
@@ -66,7 +66,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-60807213-1', 'auto');
+    ga('create', 'UA-61213311-1', 'auto');
     ga('send', 'pageview');
 
 </script>
